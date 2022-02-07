4c419f2a035ccd3c0d9e5904100a755866e15f52
Paulo Cezar Rodrigues da Silva <engenheiro.paulo.cezar@gmail.com>
2022-02-07 19:45:27 -0400

    Projeto criado

---
+++ src/main/java/br/jus/trerr/apidiarias/ApidiariasApplication.java
@@ -0,0 +1,13 @@
+package br.jus.trerr.apidiarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApidiariasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApidiariasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/jus/trerr/apidiarias/ApidiariasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.jus.trerr.apidiarias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApidiariasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
